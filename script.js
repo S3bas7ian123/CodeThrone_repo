922eca8b64ec8c3bbd2aaccb7616b3f24ec90743
Sebastian <sebastianLevi84@gmail.com>
2024-11-22 19:57:09 +0100

    Different updates. New colors for fonts, the tag main and set the style classes form js file, whit DOM Manipulation

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const titleHuge = document.querySelector('h1');
 const titleTiny = document.querySelector('h4');
 const para = document.querySelector('p');
+
+titleHuge.classList.add('setHuge');
+titleTiny.classList.add('setTiny');
