c68122abde341117c261c1a444c24928ca2c536b
Sebastian <enzorossi@gmail.com>
2024-11-24 16:28:06 +0100

    'For each icon, change the color!'

--- script.js
+++ script.js
@@ -4,12 +4,24 @@ const para = document.querySelector('p');
 const iconFacebook = document.querySelector('.fa-square-facebook');
 const iconTwitter = document.querySelector('.fa-square-x-twitter');
 const iconInstagram= document.querySelector('.fa-square-instagram');
+const icons = [iconFacebook, iconTwitter, iconInstagram];
+
+
 
 titleHuge.classList.add('setHuge');
 titleTiny.classList.add('setTiny');
 para.classList.add('setPara');
 
 
+icons.forEach((icon) => {
+    icon.addEventListener('mouseenter', () => {
+        icon.classList.add('mouseEnterIcons');
+    })
+
+    icon.addEventListener('mouseleave', ()=> {
+        icon.classList.remove('mouseEnterIcons');
+    })
+});
 
 // button:hover {
 //     color: rgba(0, 86, 173, 0.767);
