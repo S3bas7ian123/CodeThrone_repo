f64672b5394344cac3a9a6d9e72180ff86efa2d6
Sebastian <enzorossi@gmail.com>
2024-11-24 16:40:24 +0100

    Another changes

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+/* Dom manipulation = TITLES, PARAGRAPH */
 const titleHuge = document.querySelector('h1');
 const titleTiny = document.querySelector('h4');
 const para = document.querySelector('p');
+
+/* Dom manipulation = ICONS */
 const iconFacebook = document.querySelector('.fa-square-facebook');
 const iconTwitter = document.querySelector('.fa-square-x-twitter');
 const iconInstagram= document.querySelector('.fa-square-instagram');
 const icons = [iconFacebook, iconTwitter, iconInstagram];
 
+/* Dom manipulation = BUTTON */
+const buttonCool = document.querySelector('button');
 
-
+/* SETTINGS */
 titleHuge.classList.add('setHuge');
 titleTiny.classList.add('setTiny');
 para.classList.add('setPara');
 
-
+/* FUNCTIONS */
 icons.forEach((icon) => {
     icon.addEventListener('mouseenter', () => {
         icon.classList.add('mouseEnterIcons');
@@ -23,7 +28,12 @@ icons.forEach((icon) => {
     })
 });
 
-// button:hover {
-//     color: rgba(0, 86, 173, 0.767);
-//     background-color:  rgb(255, 217, 111);
-// }
+// buttonCool.addEventListener('mouseenter', () => {
+//     buttonCool.classList.add('');
+// })
+
+// buttonCool.addEventListener('mouseleave', () => {
+//     buttonCool.classList.remove('');
+// })
+
+
